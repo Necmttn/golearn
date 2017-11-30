b4d4e4384504b99cc8354b5c8707ee8557d96601
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2017-11-30 08:17:53 +0800

    omni auth added

---
+++ src/veni/auth.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// not authenticated
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		// some other error
+		panic(err.Error())
+	} else {
+		// success - call the next handler
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+// loginHandler handles the third-party login process.
+// format /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	if len(segs) < 3 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "not enough params.")
+	} else {
+		action := segs[2]
+		provider := segs[3]
+		switch action {
+		case "login":
+			log.Println("TODO handle login for", provider)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "Auth action %s not supported", action)
+		}
+	}
+}
+
+// Decides user must auth or not base on cookies.
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
--- src/veni/main.go
+++ src/veni/main.go
@@ -29,7 +29,9 @@ func main() {
 	flag.Parse()
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// get the room going.
 	go r.run()
